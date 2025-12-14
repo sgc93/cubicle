17d0b9f0563d128624b5f1bcff44b33d4ae49c89
sgc93 <smachewgedefaw@gmail.com>
2025-12-14 18:50:31 +0300

    add Inter font type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
 import "../styles/globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const interFont = Geist({
+  variable: "--font-inter",
+  subsets: ["latin"]
 });
 
 export const metadata: Metadata = {
@@ -18,17 +13,13 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${interFont.variable} antialiased`}>{children}</body>
     </html>
   );
 }
