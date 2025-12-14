03ada333fe81b8f0bf8e5a7cbd3c54082a7aa17c
sgc93 <smachewgedefaw@gmail.com>
2025-12-14 19:34:13 +0300

    update: reform layout with new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Header from "@/components/Header/Header";
+import Scene from "@/components/Scene/Scene";
+import SecondarySidebar from "@/components/SecondarySidebar/SecondarySidebar";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <span>Cubicle</span>
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Header />
+      <div className="flex-1 min-w-0 flex w-screen">
+        <Scene />
+        <SecondarySidebar />
+      </div>
     </div>
   );
 }
