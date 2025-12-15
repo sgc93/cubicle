6b3113e9ed5a17b411e6cee3f3446e1718ec95b8
sgc93 <smachewgedefaw@gmail.com>
2025-12-15 17:54:09 +0300

    update: handle initializing scene object list

--- src/components/Scene/Scene.tsx
+++ src/components/Scene/Scene.tsx
@@ -1,9 +1,21 @@
 "use client";
 
+import { useSceneStore } from "@/stores/store";
 import SceneCanvas from "./SceneCanvas";
 import SceneMenu from "./SceneMenu";
 
+import { useEffect } from "react";
+import { initialSceneData } from "@/data/initialSchema";
+
 const Scene = () => {
+  const { loadInitialScene, sceneObjects } = useSceneStore();
+
+  useEffect(() => {
+    if (sceneObjects.length === 0) {
+      loadInitialScene(initialSceneData);
+    }
+  }, [sceneObjects.length, loadInitialScene]);
+
   return (
     <div className="relative flex-1 flex">
       <SceneCanvas />
