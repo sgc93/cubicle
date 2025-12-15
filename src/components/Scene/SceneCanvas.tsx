5790e8fe560695bef89118e4c5aaedef9f711a4c
sgc93 <smachewgedefaw@gmail.com>
2025-12-15 13:21:56 +0300

    setup: basic scene elements like grid, lighting, bouding box

--- src/components/Scene/SceneCanvas.tsx
+++ src/components/Scene/SceneCanvas.tsx
@@ -1,5 +1,11 @@
-import { Grid, OrbitControls } from "@react-three/drei";
+import {
+  Grid,
+  Helper,
+  OrbitControls,
+  TransformControls
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { BoxHelper } from "three";
 
 const SceneCanvas = () => {
   const cameraProps = {
@@ -8,44 +14,33 @@ const SceneCanvas = () => {
   };
 
   return (
-    <Canvas
-      camera={cameraProps}
-      shadows
-      className="flex-1 min-w-0 w-full h-full bg-n-600"
-    >
+    <Canvas camera={cameraProps} shadows className="flex-1 min-w-0 bg-n-600">
       <ambientLight intensity={0.5} />
-      <directionalLight
-        position={[10, 10, 5]}
-        intensity={1.5}
-        castShadow
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        shadow-camera-far={50}
-        shadow-camera-left={-10}
-        shadow-camera-right={10}
-        shadow-camera-top={10}
-        shadow-camera-bottom={-10}
-      />
+      <directionalLight position={[10, 10, 5]} intensity={1.5} castShadow />
       <OrbitControls makeDefault enableDamping dampingFactor={0.05} />
-
+      <TransformControls mode={"translate"}>
+        <mesh
+          receiveShadow
+          position={[0, 0, 0]}
+          rotation={[-Math.PI / 2, 0, 0]}
+          onClick={() => console.log("mesh clicked")}
+        >
+          <torusGeometry />
+          <meshStandardMaterial color="#fff" roughness={0.8} metalness={0.2} />
+          <Helper type={BoxHelper} args={["#f78000"]} />
+        </mesh>
+      </TransformControls>
       <Grid
-        args={[20, 20, 40, 40]}
+        args={[100, 100]}
+        cellSize={1}
         cellColor="#9d9d9d"
+        sectionSize={10}
         sectionColor="#5a5a5a"
         sectionThickness={1}
-        fadeDistance={500}
         position={[0, 0.001, 0]}
+        fadeDistance={50}
+        infiniteGrid
       />
-
-      <mesh
-        receiveShadow
-        position={[0, 0, 0]}
-        rotation={[-Math.PI / 2, 0, 0]}
-        onClick={() => console.log("mesh clicked")}
-      >
-        <boxGeometry />
-        <meshStandardMaterial color="#fff" roughness={0.8} metalness={0.2} />
-      </mesh>
     </Canvas>
   );
 };
