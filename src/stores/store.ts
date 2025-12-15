ea8cadcb8f92f65f83c60f3c1bc9768c49c1cb8d
sgc93 <smachewgedefaw@gmail.com>
2025-12-15 19:37:27 +0300

    update: handle updating object data

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,14 +1,19 @@
 import { SceneData, SceneObject } from "@/types/SceneSchema";
 import { ObjectModeType } from "@/types/SceneTypes";
 import { create } from "zustand";
+import * as THREE from "three";
 
 interface SceneState {
   sceneObjects: SceneData;
   selectedObjectId: string | null;
   mode: ObjectModeType;
 
+  history: SceneData[];
+  historyIndex: number;
+
   // actions
   addObject: (obj: SceneObject) => void;
+  updateMesh: (id: string, mesh: THREE.Mesh) => void;
   setSelectedObjectId: (id: string | null) => void;
   loadInitialScene: (data: SceneData) => void;
 }
@@ -16,7 +21,9 @@ interface SceneState {
 export const useSceneStore = create<SceneState>((set, get) => ({
   sceneObjects: [],
   selectedObjectId: null,
-  mode: 'translate',
+  mode: "translate",
+  history: [],
+  historyIndex: -1,
 
   loadInitialScene: (data) =>
     set({
@@ -30,5 +37,16 @@ export const useSceneStore = create<SceneState>((set, get) => ({
     }));
   },
 
+  updateMesh: (id, mesh) => {
+    set((state) => ({
+      sceneObjects: state.sceneObjects.map((obj) => {
+        if (obj.id !== id) return obj;
+
+        return { ...obj, mesh };
+      })
+    }));
+  },
+
   setSelectedObjectId: (id) => set({ selectedObjectId: id })
+
 }));
