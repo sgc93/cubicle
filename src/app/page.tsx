6c14e629a8afb78d1ad3f150319d5599f9a6fd97
sgc93 <smachewgedefaw@gmail.com>
2025-12-16 19:21:05 +0300

    fix: make h screen sized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import SecondarySidebar from "@/components/SecondarySidebar/SecondarySidebar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black overflow-hidden">
+    <div className="flex flex-col h-screen w-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black overflow-hidden">
       <Header />
       <div className="flex-1 min-w-0 flex w-screen overflow-hidden scene-box">
         <Scene />
