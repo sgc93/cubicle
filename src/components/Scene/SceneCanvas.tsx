a7d6bd5f6876833ab31c613ee640a5713533e7bf
sgc93 <smachewgedefaw@gmail.com>
2025-12-16 19:41:55 +0300

    refactor: handle individual object rendering in child component

--- src/components/Scene/SceneCanvas.tsx
+++ src/components/Scene/SceneCanvas.tsx
@@ -1,71 +1,9 @@
 import { useSceneStore } from "@/stores/store";
-import {
-  Grid,
-  Helper,
-  OrbitControls,
-  TransformControls
-} from "@react-three/drei";
+import { Grid, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import * as THREE from "three";
-import { useRef } from "react";
-import { SceneObject } from "@/types/SceneTypes";
+import InteractiveSceneObject from "./InteractiveSceneObject";
 
-const InteractiveSceneObject = ({ object }: { object: SceneObject }) => {
-  const { setSelectedObjectId, mode, updateMesh } = useSceneStore();
-  const isSelected = useSceneStore(
-    (state) => state.selectedObjectId === object.id
-  );
-
-  const meshRef = useRef<THREE.Mesh>(null!);
-
-  const onMouseUp = () => {
-    if (meshRef.current) {
-      updateMesh(object.id, meshRef.current);
-    }
-  };
-
-  const selectObj = () => {
-    if (!isSelected) {
-      setSelectedObjectId(object.id);
-    }
-  };
-
-  const MeshComponent = (
-    <mesh
-      ref={meshRef}
-      receiveShadow
-      castShadow
-      position={[object.position.x, object.position.y, object.position.z]}
-      rotation={[object.rotation.x, object.rotation.y, object.rotation.z]}
-      scale={[object.scale.x, object.scale.y, object.scale.z]}
-      onClick={(e) => {
-        e.stopPropagation();
-        selectObj();
-      }}
-    >
-      <boxGeometry
-        args={[object.args[0] || 1, object.args[1] || 1, object.args[2] || 1]}
-      />
-      <meshStandardMaterial
-        color={object.material?.color || "#ffffff"}
-        roughness={object.material?.roughness || 0.8}
-        metalness={object.material?.metalness || 0.2}
-      />
-
-      {isSelected && <Helper type={THREE.BoxHelper} args={["#f78000"]} />}
-    </mesh>
-  );
-
-  if (isSelected) {
-    return (
-      <TransformControls object={meshRef} mode={mode} onMouseUp={onMouseUp}>
-        {MeshComponent}
-      </TransformControls>
-    );
-  }
-
-  return MeshComponent;
-};
 
 const SceneCanvas = () => {
   const { sceneObjects, setSelectedObjectId, selectedObjectId } =
