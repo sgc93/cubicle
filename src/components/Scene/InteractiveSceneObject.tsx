a4f7739e45ee4b453869982fac0e0448e0d0cb1b
sgc93 <smachewgedefaw@gmail.com>
2025-12-16 23:11:40 +0300

    update: handle displaying components based on their shape

--- src/components/Scene/InteractiveSceneObject.tsx
+++ src/components/Scene/InteractiveSceneObject.tsx
@@ -1,19 +1,74 @@
 import { useSceneStore } from "@/stores/store";
 import {
   Helper,
+  MeshDistortMaterial,
   TransformControls
 } from "@react-three/drei";
 import * as THREE from "three";
 import { useRef } from "react";
 import { SceneObject } from "@/types/SceneTypes";
+import GeometryEditor from "./GeometryEditor";
+
+const getGeometryComponent = (object: SceneObject) => {
+  switch (object.type) {
+    case "box":
+    case "mesh":
+      return (
+        <boxGeometry
+          args={[object.args[0] || 1, object.args[1] || 1, object.args[2] || 1]}
+        />
+      );
+
+    case "plane":
+      return (
+        <planeGeometry
+          args={[object.args[0] || 1, object.args[1] || 1, object.args[2] || 1]}
+        />
+      );
+
+    case "sphere":
+      return (
+        <sphereGeometry
+          args={[object.args[0] || 1, object.args[1] || 1, object.args[2] || 1]}
+        />
+      );
+
+    case "cylinder":
+      return (
+        <cylinderGeometry
+          args={[object.args[0] || 1, object.args[1] || 1, object.args[2] || 1]}
+        />
+      );
+
+    case "cone":
+      return (
+        <coneGeometry
+          args={[object.args[0] || 1, object.args[1] || 1, object.args[2] || 1]}
+        />
+      );
+
+    case "torus":
+      return (
+        <torusGeometry
+          args={[object.args[0] || 1, object.args[1] || 1, object.args[2] || 1]}
+        />
+      );
+    default:
+      return <boxGeometry args={[1, 1, 1]} />;
+  }
+};
 
 const InteractiveSceneObject = ({ object }: { object: SceneObject }) => {
   const { setSelectedObjectId, mode, updateMesh } = useSceneStore();
   const isSelected = useSceneStore(
     (state) => state.selectedObjectId === object.id
   );
-
+  const isEditing = false;
   const meshRef = useRef<THREE.Mesh>(null!);
+
+  if (isEditing && isSelected) {
+    return <GeometryEditor object={object} meshRef={meshRef} />;
+  }
 
   const onMouseUp = () => {
     if (meshRef.current) {
@@ -40,9 +95,7 @@ const InteractiveSceneObject = ({ object }: { object: SceneObject }) => {
         selectObj();
       }}
     >
-      <boxGeometry
-        args={[object.args[0] || 1, object.args[1] || 1, object.args[2] || 1]}
-      />
+      {getGeometryComponent(object)}
       <meshStandardMaterial
         color={object.material?.color || "#ffffff"}
         roughness={object.material?.roughness || 0.8}
